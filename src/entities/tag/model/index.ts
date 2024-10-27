437ac4cc386e1655548a81e8df7a67e729c3a2e5
soyoonJ <sytkfkd96@gmail.com>
2024-10-27 20:31:22 +0900

    refactor: tag types 추가

--- src/entities/tag/model/index.ts
+++ src/entities/tag/model/index.ts
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import { fetchTagsApi } from "../api"
+import { Tag } from "./types"
 
 export const useTags = () => {
-  const [tags, setTags] = useState<string[]>([])
+  const [tags, setTags] = useState<Tag[]>([])
 
   const getTags = () => {
     fetchTagsApi().then((data) => setTags(data))
