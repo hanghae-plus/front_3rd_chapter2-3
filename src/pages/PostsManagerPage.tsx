9364177800a5243f114f907df0d82cb5ba6d800f
ms-bang <ms.bang@teamsparta.co>
2024-10-27 23:28:38 +0900

    feat: 🎸 fsd에 대한 eslint 설정 추가

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
 import { Edit2, MessageSquare, Plus, Search, ThumbsDown, ThumbsUp, Trash2 } from "lucide-react"
+import { useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import {
   Button,
