8acfdfec54ddc3eabdba0bb946214044cc2c4c94
Gred <jihong.park.dev@gmail.com>
2024-10-28 23:17:24 +0900

    feat: PostManagerPage 컴포넌트 import 경로 수정

--- src/shared/ui/Dialog/index.tsx
+++ src/shared/ui/Dialog/index.tsx
@@ -1,9 +1,11 @@
 // 대화상자 컴포넌트
-// export {
-//   Root as Dialog,
-//   Overlay as DialogOverlay,
-//   Portal as DialogPortal,
-//   Trigger as DialogTrigger,
-// } from '@radix-ui/react-dialog';
+export {
+  Root as Dialog,
+  Overlay as DialogOverlay,
+  Portal as DialogPortal,
+  Trigger as DialogTrigger,
+} from '@radix-ui/react-dialog';
 
-export { Root } from '@radix-ui/react-dialog';
+export { DialogContent } from './DialogContent';
+export { DialogHeader } from './DialogHeader';
+export { DialogTitle } from './DialogTitle';
