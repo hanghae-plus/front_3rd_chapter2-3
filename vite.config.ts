3f22a4d1bf49266e16d7cc6896a09170e8e2dad3
Gred <jihong.park.dev@gmail.com>
2024-10-28 23:12:34 +0900

    fix: tsconfig 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import * as path from 'path';
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: { alias: [{ find: '~/', replacement: path.resolve(__dirname, 'src') }] },
   server: {
     proxy: {
-      "/api": {
+      '/api': {
         // target: 'https://jsonplaceholder.typicode.com',
-        target: "https://dummyjson.com",
+        target: 'https://dummyjson.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-})
+});
