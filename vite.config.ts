d85bc7adea70cbb462ec6754bb12af2ccb9b04e2
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-28 18:35:35 +0900

    🐛 fix :: 프록시 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         // target: 'https://jsonplaceholder.typicode.com',
         target: "https://dummyjson.com",
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
