81bb33dddf5ad4f3315dbd967f2d7d3677e0cc7e
Gred <jihong.park.dev@gmail.com>
2024-10-28 23:53:51 +0900

    fix: vite alias 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 
+console.log('path', path.resolve(__dirname, 'src'));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: [{ find: '~/', replacement: path.resolve(__dirname, 'src') }] },
+  resolve: { alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }] },
   server: {
     proxy: {
       '/api': {
