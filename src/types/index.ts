77823e854165119bc71724f6abd6cedf5fb55577
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-10-28 09:34:16 +0900

    🏷️ feat :: 전역으로 사용되는 타입을 정의

---
+++ src/types/index.ts
@@ -0,0 +1,63 @@
+export interface Post {
+  id: number
+  title: string
+  body: string
+  userId: number
+  tags: string[]
+  reactions?: {
+    likes: number
+    dislikes: number
+  }
+  author?: User
+}
+
+export interface User {
+  id: number
+  username: string
+  image: string
+  firstName?: string
+  lastName?: string
+  age?: number
+  email?: string
+  phone?: string
+  address?: {
+    address: string
+    city: string
+    state: string
+  }
+  company?: {
+    name: string
+    title: string
+  }
+}
+
+export interface Comment {
+  id: number
+  body: string
+  postId: number
+  userId: number
+  likes: number
+  user: User
+}
+
+export interface Tag {
+  url: string
+  slug: string
+}
+
+export interface PaginatedResponse<T> {
+  posts: T[]
+  total: number
+}
+
+export interface NewPost {
+  title: string
+  body: string
+  userId: number
+}
+
+export interface NewComment {
+  body: string
+  postId: number | null
+  userId: number
+}
