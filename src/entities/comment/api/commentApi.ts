12c79cd283352e2870550de9d7f112366855b9cb
JayeHa <jwha94@gmail.com>
2024-10-29 17:51:56 +0900

    feat: commentApi.addComment 추가

--- src/entities/comment/api/commentApi.ts
+++ src/entities/comment/api/commentApi.ts
@@ -1,5 +1,5 @@
 import { safeFetch } from "../../../shared/lib"
-import { FetchCommentResponse } from "../model/types"
+import { Comment, FetchCommentResponse, NewComment } from "../model/types"
 
 export const commentApi = {
   /* 댓글 가져오기 **/
@@ -12,6 +12,16 @@ export const commentApi = {
       return response
     } catch (error) {
       console.error("댓글 가져오기 오류:", error)
+    }
+  },
+
+  /** 댓글 추가 */
+  addComment: async (newComment: NewComment) => {
+    try {
+      const response = await safeFetch.post<Comment>("/api/comments/add", newComment)
+      return response
+    } catch (error) {
+      console.error("댓글 추가 오류:", error)
     }
   },
 }
