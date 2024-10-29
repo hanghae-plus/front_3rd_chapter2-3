f36f14e577a2b9bbf20ac1bd565af93d71e276d7
jhkim <jhkim@enterprise1.co.kr>
2024-10-29 17:53:11 +0900

    PostsMngPg : FSD 구조 나누기

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -364,11 +364,10 @@ const PostsManager = () => {
                   {post.tags.map((tag) => (
                     <span
                       key={tag}
-                      className={`px-1 text-[9px] font-semibold rounded-[4px] cursor-pointer ${
-                        selectedTag === tag
+                      className={`px-1 text-[9px] font-semibold rounded-[4px] cursor-pointer ${selectedTag === tag
                           ? "text-white bg-blue-500 hover:bg-blue-600"
                           : "text-blue-800 bg-blue-100 hover:bg-blue-200"
-                      }`}
+                        }`}
                       onClick={() => {
                         setSelectedTag(tag)
                         updateURL()
