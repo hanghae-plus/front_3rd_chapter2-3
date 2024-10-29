0fc9788c0dffa5fd2ea15ed02a6d8d243082ce55
John-Kim93 <dhkdhd123@gmail.com>
2024-10-29 23:28:12 +0900

    chore : jotai, tanstack-query 적용

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,12 @@ import tseslint from "typescript-eslint"
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, "@feature-sliced"],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "@feature-sliced",
+      "plugin:@tanstack/eslint-plugin-query/recommended",
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
