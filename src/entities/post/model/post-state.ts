a86199c042b7fbfc8369ca19238276ee0511d7b8
이동아 <christar99@naver.com>
2024-10-29 13:59:51 +0900

    post - zustand이용 전역선언

---
+++ src/entities/post/model/post-state.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+import { PostType } from "./post-type";
+
+interface PostListState {
+  postList: PostType[];
+  addPostState: (post: PostType) => void;
+}
+
+export const postListState = create<PostListState>(set => ({
+  postList: [],
+  addPostState: post => set(prev => ({ ...prev, post })),
+}));
