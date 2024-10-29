361823c8433b94cff03cb9fcd4ecbc395673bb7b
이동아 <christar99@naver.com>
2024-10-29 18:30:28 +0900

    헤더푸터 위치조정, overlayProvider 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./widgets/layout/ui/header/Header.tsx";
+import Footer from "./widgets/layout/ui/footer/Footer.tsx";
+import PostsManagerPage from "./pages/PostsManagerPage.tsx";
+import { OverlayProvider } from "overlay-kit";
 
 const App = () => {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
+      <OverlayProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </OverlayProvider>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
