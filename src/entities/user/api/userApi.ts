3403adda6e6bf7aebf038dba8ca6a3096b0adf53
ms-bang <ms.bang@teamsparta.co>
2024-10-30 01:35:14 +0900

    fix: 🐛 user respone type 추가

--- src/entities/user/api/userApi.ts
+++ src/entities/user/api/userApi.ts
@@ -1,9 +1,10 @@
 import { baseApi } from "../../../shared/api/base"
+import { UserParams, UserResponse } from "../model/types"
 
 export const userApi = {
   get: {
-    users: async ({ limit = 0, select = "username,image" }: { limit?: number; select?: string } = {}) => {
-      const response = await baseApi.get("/users", {
+    users: async ({ limit = 0, select = "username,image" }: UserParams) => {
+      const response = await baseApi.get<UserResponse>("/users", {
         params: {
           limit,
           select,
--- src/entities/user/model/types.ts
+++ src/entities/user/model/types.ts
@@ -1,1 +1,17 @@
+export interface UserResponse {
+  users: User[]
+  total: number
+  skip: number
+  limit: number
+}
 
+interface User {
+  id: number
+  username: string
+  image: string
+}
+
+export interface UserParams {
+  limit?: number
+  select?: string
+}
