8c84c5f1ef48911a6184666ea232a87d608fef9c
cruellaDev <cruellaDevLee@gmail.com>
2024-10-29 17:59:59 +0900

    * feat: post api, type 추가

---
+++ src/entities/post/types/index.ts
@@ -0,0 +1,31 @@
+export interface Post {
+  id: number
+  title: string
+  body: string
+  tags: string[]
+  reactions: {
+    likes: number
+    dislikes: number
+  }
+  views: number
+  userId: number
+  author: Author | undefined
+}
+
+export interface Author {
+  id: number
+  username: string
+  image: string
+}
+
+export interface Page {
+  limit: number
+  skip: number
+}
+
+export interface NewPost {
+  title: string
+  body: string
+  userId: number
+  tags: string[]
+}
