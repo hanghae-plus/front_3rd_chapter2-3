e0fc4406d2c0d3ba1ef31eefef53fb1cfea122bf
soyoonJ <sytkfkd96@gmail.com>
2024-10-30 00:20:11 +0900

    chore: QueryClientProvider 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import { BrowserRouter as Router } from "react-router-dom"
 import Header from "./widgets/layout/ui/Header.tsx"
 import Footer from "./widgets/layout/ui/Footer.tsx"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
 
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
 
-          <Footer />
-        </main>
-      </div>
-    </Router>
+            <Footer />
+          </main>
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
