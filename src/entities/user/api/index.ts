3940ea97e56aa79761086d790fd09b5dacc87675
cruellaDev <cruellaDevLee@gmail.com>
2024-10-30 13:39:38 +0900

    * feat: user 사용자 정보 가져오기 api 추가

--- src/entities/user/api/index.ts
+++ src/entities/user/api/index.ts
@@ -15,3 +15,14 @@ export const fetchUsersApi = async (): Promise<UsersData> => {
     throw new Error(`사용자 가져오기 오류: ${error}`)
   }
 }
+
+// 사용자 정보 가져오기
+export const fetchUserApi = async (userId: number): Promise<User> => {
+  try {
+    const response = await fetch(`/api/users/${userId}`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw new Error(`사용자 정보 가져오기 오류: ${error}`)
+  }
+}
