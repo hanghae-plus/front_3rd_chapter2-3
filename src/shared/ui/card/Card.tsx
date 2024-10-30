10ee54f192b79f4ffff697b866a88cfef8e62ebe
lunashp <hijnshp@gmail.com>
2024-10-30 15:54:15 +0900

    feat: 분리한 컴포넌트 적용

--- src/shared/ui/card/Card.tsx
+++ src/shared/ui/card/Card.tsx
@@ -1,6 +1,10 @@
 import { forwardRef } from "react"
 
-// interface CardProps extends React.HTMLAttributes<HTMLDivElement>{}
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string
+}
 
-export const Card = forwardRef<HTMLDivElement>((props, ref) => <div ref={ref} className="card-class" {...props} />)
+export const Card = forwardRef<HTMLDivElement, CardProps>((props, ref) => (
+  <div ref={ref} className="card-class" {...props} />
+))
 Card.displayName = "Card"
