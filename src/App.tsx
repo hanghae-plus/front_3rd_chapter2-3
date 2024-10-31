613ab65e7f7769f7f2545337dba9d9053da0b3f7
Seri Park <parkseridev@gmail.com>
2024-10-31 16:07:50 +0900

    chore: tanstack-query, devtools 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
+import Header from "./widgets/layout/ui/Header.tsx"
+import Footer from "./widgets/layout/ui/Footer.tsx"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+          <ReactQueryDevtools />
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
