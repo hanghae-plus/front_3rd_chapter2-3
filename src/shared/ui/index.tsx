38eb1d5b3246efcb568cb6226b2190753acb07ba
JANG DONG JIN <jdwls27@naver.com>
2024-11-01 02:26:28 +0900

    shared 정의

--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -1,7 +1,7 @@
 export { Button } from "./Button"
 export { Input } from "./Input"
 export { Card, CardContent, CardHeader, CardTitle } from "./Card"
-export { Textarea } from ".//TextArea"
+export { Textarea } from "./TextArea"
 export { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./Select"
 export { Dialog, DialogContent, DialogHeader, DialogTitle } from "./Dialog"
 export { TableBody, TableCell, TableHead, TableHeader, TableRow } from "./Table"
