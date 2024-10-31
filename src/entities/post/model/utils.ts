9bccabbe5b6595dbf6f11c9ea3672f8fd22d32bf
cruellaDev <cruellaDevLee@gmail.com>
2024-10-31 19:15:08 +0900

    * feat: post/utils 기능 추가 - attachAuthorFromUser

--- src/entities/post/model/utils.ts
+++ src/entities/post/model/utils.ts
@@ -8,12 +8,19 @@ export function attachAuthorsFromUsers(posts: Post[], users: User[]): Post[] {
   }))
 }
 
+export function attachAuthorFromUser(post: Post, user: User) {
+  return {
+    ...post,
+    author: { id: post.userId, username: user.username, image: user.image },
+  }
+}
+
 export function addToPosts(posts: Post[], newPost: Post): Post[] {
-  return [newPost, ...posts]
+  return [{ ...newPost, views: 0 }, ...posts]
 }
 
 export function updateInPosts(posts: Post[], updatedPost: Post) {
-  return posts.map((post) => (post.id === updatedPost.id ? updatedPost : post))
+  return posts.map((post) => (post.id === updatedPost.id ? { ...post, ...updatedPost } : post))
 }
 
 export function removeFromPosts(posts: Post[], postId: number): Post[] {
