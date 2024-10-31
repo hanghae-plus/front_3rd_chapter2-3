a986ceb62083a1ebdb0f6fc438c769e897f8a6d2
JANG DONG JIN <jdwls27@naver.com>
2024-11-01 02:32:14 +0900

    App 작업 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
+import Header from "./shared/ui/Header.tsx"
+import Footer from "./shared/ui/Footer.tsx"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import PostPage from "./pages/PostPage.tsx"
 
 const App = () => {
   const queryClient = new QueryClient()
@@ -12,7 +13,8 @@ const App = () => {
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-grow container mx-auto px-4 py-8">
-            <PostsManagerPage />
+            {/* <PostsManagerPage /> */}
+            <PostPage />
           </main>
           <Footer />
         </div>
