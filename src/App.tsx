dafeec62b02e0359e4879934fc6ea38ca3d49dc4
이동아 <christar99@naver.com>
2024-11-01 01:39:19 +0900

    overlay-kit, rq provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import { OverlayProvider } from "overlay-kit";
+
 import Header from "./widgets/layout/ui/header/Header.tsx";
 import Footer from "./widgets/layout/ui/footer/Footer.tsx";
 import PostsManagerPage from "./pages/PostsManagerPage.tsx";
-import { OverlayProvider } from "overlay-kit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClinet = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: Infinity },
+  },
+});
 
 const App = () => {
   return (
-    <Router>
-      <OverlayProvider>
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-grow container mx-auto px-4 py-8">
-            <PostsManagerPage />
-          </main>
-          <Footer />
-        </div>
-      </OverlayProvider>
-    </Router>
+    <QueryClientProvider client={queryClinet}>
+      <Router>
+        <OverlayProvider>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow container mx-auto px-4 py-8">
+              <PostsManagerPage />
+            </main>
+            <Footer />
+          </div>
+        </OverlayProvider>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
