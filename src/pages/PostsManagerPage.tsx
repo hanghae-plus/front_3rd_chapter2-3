e77ed4738b0fa372e433cb596f13eb139fcacf2e
Kang Seokhyun <yoo2yoo2yoo2@gmail.com>
2024-11-01 01:51:08 +0900

    ♻️문서 분리

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -1,12 +1,12 @@
-import PostsManagerHeader from "../features/post/ui/PostsManagerHeader"
-import PostsManagerContent from "../features/post/ui/PostsManagerContent"
-import AddPostDialog from "../features/post/ui/AddPostDialog"
-import UpdatePostDialog from "../features/post/ui/UpdatePostDialog"
+import PostsManagerHeader from "../widgets/ui/PostsManagerHeader"
+import PostsManagerContent from "../widgets/ui/PostsManagerContent"
 import AddCommentDialog from "../features/comment/ui/AddCommentDialog"
 import UpdateCommentDialog from "../features/comment/ui/UpdateCommentDialog"
 import PostDetailDialog from "../features/post/ui/PostDetailDialog"
 import UserModal from "../features/user/ui/UserModal"
 import { Card } from "../shared/ui/Card"
+import AddPostDialog from "../features/post/ui/AddPostDialog"
+import UpdatePostDialog from "../features/post/ui/UpdatePostDialog"
 
 const PostsManager = () => {
   return (
