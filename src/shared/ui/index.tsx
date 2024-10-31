ed30bf2d23ed836d45fe41b56fa3b966b04fae6b
bong <borobong230@gmail.com>
2024-11-01 08:59:24 +0900

    :bug: [Fix] lint 경고 수정 #main

--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -1,7 +1,32 @@
-export * from "./dialog/Dialog"
-export * from "./table/Table"
-export * from "./input/Input"
-export * from "./select/Select"
-export * from "./button/Button"
-export * from "./card/Card"
-export * from "./textarea/Textarea"
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog/Dialog"
+import { Table, TableHeader, TableBody, TableHead, TableRow, TableCell } from "./table/Table"
+import { Input } from "./input/Input"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./select/Select"
+import { Button } from "./button/Button"
+import { Card, CardContent, CardHeader, CardTitle } from "./card/Card"
+import { Textarea } from "./textarea/Textarea"
+
+export {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  Table,
+  TableHeader,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableCell,
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  Textarea,
+}
