ba00410d75e12ad93aaa0cc1d34651597aff8ed7
soyoonJ <sytkfkd96@gmail.com>
2024-11-01 00:51:25 +0900

    test: 테스트 초기화 위한 jotai Provider 추가

--- tests/basic.test.tsx
+++ tests/basic.test.tsx
@@ -1,14 +1,15 @@
-import { describe, it, expect, beforeAll, afterEach, afterAll } from "vitest"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import "@testing-library/jest-dom"
 import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
+import { Provider } from "jotai"
 import { http, HttpResponse } from "msw"
 import { setupServer } from "msw/node"
-import { MemoryRouter } from "react-router-dom"
-import PostsManager from "../src/pages/PostsManagerPage"
 import * as React from "react"
-import "@testing-library/jest-dom"
+import { MemoryRouter } from "react-router-dom"
+import { afterAll, afterEach, beforeAll, describe, expect, it } from "vitest"
+import PostsManager from "../src/pages/PostsManagerPage"
 import { TEST_POSTS, TEST_SEARCH_POST, TEST_USERS } from "./mockData"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 // MSW 서버 설정
 const server = setupServer(
@@ -41,7 +42,9 @@ const server = setupServer(
 )
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }))
-afterEach(() => server.resetHandlers())
+afterEach(() => {
+  server.resetHandlers()
+})
 afterAll(() => server.close())
 
 // 테스트에 공통으로 사용될 render 함수
@@ -50,9 +53,11 @@ const renderPostsManager = () => {
 
   return render(
     <QueryClientProvider client={queryClient}>
-      <MemoryRouter>
-        <PostsManager />
-      </MemoryRouter>
+      <Provider>
+        <MemoryRouter>
+          <PostsManager />
+        </MemoryRouter>
+      </Provider>
     </QueryClientProvider>,
   )
 }
