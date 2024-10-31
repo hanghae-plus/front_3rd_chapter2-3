881212f26985b4afc5716e839211c39fc790056b
JANG DONG JIN <jdwls27@naver.com>
2024-11-01 02:26:02 +0900

    widgets 정의

---
+++ src/widgets/ui/modal/index.tsx
@@ -0,0 +1,27 @@
+import PostDetailDialog from "../../../entities/posts/ui/PostDetailDialog"
+import CommentsAddDialog from "../../../features/comments/components/CommentsAddDialog"
+import CommentsEditDialog from "../../../features/comments/components/CommentsEditDialog"
+import PostAddDialog from "../../../features/posts/components/PostAddDialog"
+import PostEditDialog from "../../../features/posts/components/PostEditDialog"
+import UserModalDialog from "../../../features/users/components/UserModalDialog"
+
+const Modal = () => {
+  return (
+    <div>
+      {/* 게시물 추가 대화상자 */}
+      <PostAddDialog />
+      {/* 게시물 수정 대화상자 */}
+      <PostEditDialog />
+      {/* 댓글 추가 대화상자 */}
+      <CommentsAddDialog />
+      {/* 댓글 수정 대화상자 */}
+      <CommentsEditDialog />
+      {/* 게시물 상세 보기 대화상자 */}
+      <PostDetailDialog />
+      {/* 사용자 모달 */}
+      <UserModalDialog />
+    </div>
+  )
+}
+
+export default Modal
---
+++ src/widgets/ui/search/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react"
+import PostAddButton from "../../../features/posts/components/PostAddButton"
+import PostTable from "../../../features/posts/components/PostTable"
+import PostPagination from "../../../pages/PostPagination"
+import PostSearchBar from "../../../pages/PostSearchBar"
+import { CardContent, CardHeader, CardTitle } from "../../../shared/ui/Card"
+import usePost from "../../../features/posts/hooks/usePost"
+
+const Search = () => {
+  const { loading, updateURL } = usePost()
+  useEffect(() => {}, [loading])
+
+  useEffect(() => {
+    updateURL()
+  }, [location.search])
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between">
+          <span>게시물 관리자</span>
+          {/* 게시물 추가 버튼 */}
+          <PostAddButton />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-4">
+          {/* 검색 및 필터 컨트롤 */}
+          <PostSearchBar />
+          {/* 게시물 테이블 renderPostTable()은 사용되는 컴포넌트에서 useEffect안에 추가 예정*/}
+          {loading ? <div className="flex justify-center p-4">로딩 중...</div> : <PostTable />}
+
+          {/* 페이지네이션 */}
+          <PostPagination />
+        </div>
+      </CardContent>
+    </>
+  )
+}
+
+export default Search
