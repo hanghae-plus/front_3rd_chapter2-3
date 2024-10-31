b412a6062faf99d092b326f27a83499634a3d06a
Bong <borobong230@gmail.com>
2024-11-01 05:34:49 +0900

    :wrench: [Chore] queryClient Provider 설정 추가 #main

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { BrowserRouter as Router } from "react-router-dom"
 import Header from "./entities/ui/layout/Header.tsx"
 import Footer from "./entities/ui/layout/Footer.tsx"
 import { PostManagerPage } from "./pages/ui/manager/PostManagerPage.tsx"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
