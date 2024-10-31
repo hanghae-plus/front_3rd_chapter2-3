1a9bb41724759c72d4389ac12c470ac3e065481a
Seri Park <parkseridev@gmail.com>
2024-11-01 03:40:15 +0900

    refactor: queryClient 기본 리프레시, 캐시 설정 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import PostsManagerPage from "./pages/PostsManagerPage.tsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+      gcTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+})
 
 const App = () => {
   return (
