4bde54728e62c09be9d3f458c688d0aa245213ca
윤성용 <iop017779@gmail.com>
2024-11-01 03:43:38 +0900

    refactor : pages api 폴더 추가

---
+++ src/pages/api/useQueryPostsAndUsers.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query"
+import { store } from "../../../entities/post/model/store.ts"
+import { getPostsAndUsers, getPostsByTagAndUsers } from "../../../entities/post/api"
+import { getPostsWithAuthors } from "../../../entities/post/model/getPostsWithAuthors.ts"
+import { useEffect } from "react"
+
+export const useQueryPostsAndUsers = (limit: number, skip: number, tag: string) => {
+  const { setPosts, setTotal, setIsLoading } = store.getState()
+
+  const { isLoading, isFetching } = useQuery(
+    ["get-posts-and-users", limit, skip, tag],
+    () => (tag === "all" || !tag ? getPostsAndUsers(limit, skip) : getPostsByTagAndUsers(tag)),
+    {
+      onSuccess: (data) => {
+        setPosts(getPostsWithAuthors(data.postsData.posts, data.usersData.users))
+        setTotal(data.postsData.total)
+      },
+      onSettled: () => setIsLoading(false),
+    },
+  )
+
+  useEffect(() => {
+    setIsLoading(isLoading || isFetching)
+  }, [isLoading, isFetching])
+}
