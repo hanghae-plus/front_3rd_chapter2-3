fa53c9915642a115a1021f836e06f9fa69bd3f37
백광현 <bgh9651@gmail.com>
2024-10-31 21:57:51 +0900

    refactor: unused code remove

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router } from "react-router-dom"
 import Header from "../widgets/layouts/ui/Header"
 import Footer from "../widgets/layouts/ui/Footer"
 import PostsPage from "../pages/PostsPage"
-import { PostsManagerPage } from "../pages/PostsManagerPage"
 
 const App = () => {
   return (
@@ -10,7 +9,6 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow container mx-auto px-4 py-8">
-          {/* <PostsManagerPage /> */}
           <PostsPage />
         </main>
         <Footer />
