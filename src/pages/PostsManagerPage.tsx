eb9aafa796a7162ec2e9658c0737f2bbea12de7e
정원 <jwha94@gmail.com>
2024-11-01 01:11:36 +0900

    feat: UserModal 추가

--- src/pages/PostsManagerPage.tsx
+++ src/pages/PostsManagerPage.tsx
@@ -45,6 +45,7 @@ import {
   TextHighlighter,
 } from "../shared/ui"
 import { PostDetailDialog } from "../widgets/post"
+import { UserModal } from "../widgets/user"
 
 const PostsManager = () => {
   const {
@@ -420,46 +421,11 @@ const PostsManager = () => {
 
       {/* 사용자 모달 */}
       {selectedUser && (
-        <Dialog open={showUserModal} onOpenChange={setShowUserModal}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>사용자 정보</DialogTitle>
-            </DialogHeader>
-            <div className="space-y-4">
-              <img
-                src={selectedUser?.image}
-                alt={selectedUser?.username}
-                className="w-24 h-24 rounded-full mx-auto"
-              />
-              <h3 className="text-xl font-semibold text-center">
-                {selectedUser?.username}
-              </h3>
-              <div className="space-y-2">
-                <p>
-                  <strong>이름:</strong> {selectedUser?.firstName}{" "}
-                  {selectedUser?.lastName}
-                </p>
-                <p>
-                  <strong>나이:</strong> {selectedUser?.age}
-                </p>
-                <p>
-                  <strong>이메일:</strong> {selectedUser?.email}
-                </p>
-                <p>
-                  <strong>전화번호:</strong> {selectedUser?.phone}
-                </p>
-                <p>
-                  <strong>주소:</strong> {selectedUser?.address?.address},{" "}
-                  {selectedUser?.address?.city}, {selectedUser?.address?.state}
-                </p>
-                <p>
-                  <strong>직장:</strong> {selectedUser?.company?.name} -{" "}
-                  {selectedUser?.company?.title}
-                </p>
-              </div>
-            </div>
-          </DialogContent>
-        </Dialog>
+        <UserModal
+          open={showUserModal}
+          onOpenChange={setShowUserModal}
+          selectedUser={selectedUser}
+        />
       )}
     </Card>
   )
