8eb1354019fd812fe7bb6ad8a0906b2f464d3d55
hyunjin.noh <hyunjin.noh@saltlux.com>
2024-10-31 22:42:17 +0900

    [refactor] 개발간에 실수로 넣은 features 제거

--- src/features/postList/index.ts
+++ src/features/postList/index.ts
@@ -1,4 +1,3 @@
 export type { PostsListState } from "./model/types"
 export { usePostsList } from "./model/usePostList"
 export { PostList } from "./ui/PostList"
-export { TagList } from "./ui/TagList"
