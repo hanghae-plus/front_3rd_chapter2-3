fa7f687da436fd960144d9a8338ca19a18190499
hyunjin.noh <hyunjin.noh@saltlux.com>
2024-10-31 23:28:31 +0900

    [etc] lint 설정 파일 수정

--- eslint.config.js
+++ eslint.config.js
@@ -4,14 +4,16 @@ import reactRefresh from "eslint-plugin-react-refresh"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
+export default [
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, "@feature-sliced"],
     files: ["**/*.{ts,tsx}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, "@feature-sliced"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
@@ -22,4 +24,4 @@ export default tseslint.config(
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   },
-)
+]
