a05c93df1b1c071be52ad92483d3c1949b3dcca3
Ensil-dev <lasags@naver.com>
2024-11-01 10:02:07 +0900

    fix: createSearchParams > object.entries 사용

--- src/features/post/lib/createSearchParams.ts
+++ src/features/post/lib/createSearchParams.ts
@@ -3,12 +3,20 @@ import { PostListParams } from "../model/type"
 export const createSearchParams = (params: PostListParams) => {
   const searchParams = new URLSearchParams()
 
-  params.skip && searchParams.set("skip", params.skip.toString())
-  params.limit && searchParams.set("limit", params.limit.toString())
-  params.searchQuery && searchParams.set("search", params.searchQuery)
-  params.sortKey && searchParams.set("sortKey", params.sortKey)
-  params.sortOrder && searchParams.set("sortOrder", params.sortOrder)
-  params.selectedTag && searchParams.set("tag", params.selectedTag)
+  const paramMapping = {
+    skip: params.skip?.toString(),
+    limit: params.limit?.toString(),
+    search: params.searchQuery,
+    sortKey: params.sortKey,
+    sortOrder: params.sortOrder,
+    tag: params.selectedTag,
+  }
+
+  Object.entries(paramMapping).forEach(([key, value]) => {
+    if (value) {
+      searchParams.set(key, value)
+    }
+  })
 
   return searchParams
 }
