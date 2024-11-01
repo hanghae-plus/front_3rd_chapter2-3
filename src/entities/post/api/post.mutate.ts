b085bbf6f72e8090990b2d9d7933e246c0a350b3
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-11-01 09:47:43 +0900

    feat: tanstack-query 추가

---
+++ src/entities/post/api/post.mutate.ts
@@ -0,0 +1,4 @@
+export const mutateKeys = {
+  DELETE_POST_KEY: (id: number) => ["DELETE_POST_KEY", id],
+  PUT_POST_KEY: (id: number) => ["PUT_POST_KEY", id],
+}
---
+++ src/entities/post/api/post.query.ts
@@ -0,0 +1,3 @@
+export const queryKeys = {
+  FETCH_POSTS_KEY: (limit: string, skip: string) => ["FETCH_POSTS_KEY", limit, skip],
+}
