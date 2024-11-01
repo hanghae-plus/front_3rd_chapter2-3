c92716c288ca76d78d95f9fbef8950773d2e1a0b
codeeon <yeon.develop@gmail.com>
2024-11-01 12:26:07 +0900

    Refactor: 타입 지정

---
+++ src/entities/comment/index.ts
@@ -0,0 +1,7 @@
+export * from "./model/comment";
+
+export * from "./api/get-comment-list";
+export * from "./api/post-comment";
+export * from "./api/update-comment";
+export * from "./api/delete-comment";
+export * from "./api/patch-like-comment";
---
+++ src/entities/post/index.ts
@@ -0,0 +1,8 @@
+export * from "./model/post";
+
+export * from "./api/get-tag-list";
+export * from "./api/get-post-list";
+export * from "./api/get-searched-post-list";
+export * from "./api/post-new-post";
+export * from "./api/update-post";
+export * from "./api/delete-post";
