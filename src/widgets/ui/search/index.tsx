789d6a993643d1ded1f21ea7d0afe4e47acf84b2
dongjang <jdwls27@naver.com>
2024-11-01 15:22:21 +0900

    명칭 변경

--- src/widgets/ui/search/index.tsx
+++ src/widgets/ui/search/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react"
 import PostAddButton from "../../../features/posts/components/PostAddButton"
 import PostTable from "../../../features/posts/components/PostTable"
-import PostPagination from "../pagination/PostPagination"
-import PostSearchBar from "../searchBar/PostSearchBar"
+import PostPagination from "../pagination"
+import PostSearchBar from "../searchBar"
 import { CardContent, CardHeader, CardTitle } from "../../../shared/ui/Card"
 import usePost from "../../../features/posts/hooks/usePost"
 
