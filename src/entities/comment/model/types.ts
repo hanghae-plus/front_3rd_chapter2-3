345b243d27ae78678d97c57c8883235510d0123a
iammiori <immioriii@gmail.com>
2024-11-01 09:30:39 +0900

    refactor: prevent comment import cycle

--- src/entities/comment/model/types.ts
+++ src/entities/comment/model/types.ts
@@ -1,18 +1,17 @@
-export interface Comment {
+//https://feature-sliced.design/kr/docs/guides/examples/types
+export interface Comment<
+  TUser extends { id: number; username: string; fullName: string }
+> {
   id: number;
   body: string;
   postId: number;
   likes: number;
-  user: {
-    // todo: 아 User 가져오고 싶다.
-    id: number;
-    username: string;
-    fullName: string;
-  };
+  user: Pick<TUser, 'id' | 'username' | 'fullName'>;
 }
-
-export interface CommentResponse {
-  comments: Comment[];
+export interface CommentResponse<
+  TUser extends { id: number; username: string; fullName: string }
+> {
+  comments: Comment<TUser>[];
   total: number;
   skip: number;
   limit: number;
